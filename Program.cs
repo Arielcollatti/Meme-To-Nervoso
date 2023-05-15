4d35518fe0a955d1103eda29233da590d1fba3f3
arielcollatti <colatti2017@gmail.com>
2023-05-15 12:27:09 -0300

    Exercicio realizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("--- Meme to nervoso ---\n");
+
+string digite, MemeToNervoso;
+
+Console.Write("Usuário, o que você pensa sobre mim? ");
+digite = Console.ReadLine()!;
+
+MemeToNervoso =
+    $"{digite.Remove(10)}... NÃO, PERA. \n Tô nervoso";
+
+Console.WriteLine($"{MemeToNervoso}");
